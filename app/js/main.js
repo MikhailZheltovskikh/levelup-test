a44b8a21211bb42da9883e0989791b91f6140c43
Mikhail_Zheltovskikh <misha14051994@mail.ru>
2021-10-27 15:19:27 +0500

    updete v2

--- app/js/main.js
+++ app/js/main.js
@@ -16,12 +16,8 @@ function getTimeRemaining(endtime) {
    var t = Date.parse(endtime) - Date.parse(new Date());
    var seconds = Math.floor((t / 1000) % 60);
    var minutes = Math.floor((t / 1000 / 60) % 60);
-   // var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-   // var days = Math.floor(t / (1000 * 60 * 60 * 24));
    return {
      total: t,
-   //   days: days,
-   //   hours: hours,
      minutes: minutes,
      seconds: seconds
    };
@@ -29,8 +25,6 @@ function getTimeRemaining(endtime) {
  
  function initializeClock(id, endtime) {
    var clock = document.getElementById(id);
-   // var daysSpan = clock.querySelector(".days");
-   // var hoursSpan = clock.querySelector(".hours");
    var minutesSpan = clock.querySelector(".minutes");
    var secondsSpan = clock.querySelector(".seconds");
  
@@ -43,8 +37,7 @@ function getTimeRemaining(endtime) {
        initializeClock('countdown', deadline);
      }
  
-   //   daysSpan.innerHTML = t.days;
-   //   hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+
      minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
      secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
    }
